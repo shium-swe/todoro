46968e784a5bc4ebde32f42fb22ce17e310263d1
shium-swe <shi.mashud@gmail.com>
2022-06-28 00:35:41 -0400

    todoro v1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+import {
+  Box,
+  Heading,
+  Text,
+  FormControl,
+  FormHelperText,
+  Input,
+  InputGroup,
+  InputRightElement,
+  VStack,
+  HStack,
+  List,
+  ListItem,
+  Button,
+  Checkbox,
+} from "@chakra-ui/react";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [defaultText, setDefaultText] = useState("");
+  const todoText = useRef();
+
+  useEffect(() => {
+    const existingTodos = localStorage.getItem("todos");
+    setTodos(existingTodos ? JSON.parse(existingTodos) : []);
+  }, []);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const nextTodos = [...todos, todoText.current.value];
+    setTodos(nextTodos);
+    localStorage.setItem("todos", JSON.stringify(todos));
+    setDefaultText("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box w="100vw" h="100vh" m={0} p={5} display="flex" justifyContent="center">
+      <VStack w="60vw" spacing={5}>
+        <Heading as="h1" fontSize="6rem" mt={4} mb={8}>
+          todoro
+        </Heading>
+        <Box w="80%">
+          <form onSubmit={handleSubmit}>
+            <FormControl>
+              <HStack>
+                <InputGroup>
+                  <Input
+                    id="todo__input"
+                    variant="filled"
+                    placeholder="'Clean room'"
+                    size="lg"
+                    type="text"
+                    value={defaultText}
+                    onChange={(event) => setDefaultText(event.target.value)}
+                    ref={todoText}
+                  />
+                  <InputRightElement width="8rem" mt={1} padding={2}>
+                    <Button type="submit" h="1.75rem" variant="solid">
+                      Add todo
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+              </HStack>
+              <FormHelperText>Enter your todo item here.</FormHelperText>
+            </FormControl>
+          </form>
+        </Box>
+        <List spacing={3}>
+          {todos.map((todo) => (
+            <ListItem
+              key={todo}
+              _hover={{
+                bgColor: "gray.100",
+                transition: "all 300ms ease",
+              }}
+              px={10}
+              py={3}
+            >
+              <HStack>
+                <Checkbox
+                  aria-label="Toggle todo"
+                  size="lg"
+                  colorScheme="green"
+                  spacing="1.5rem"
+                >
+                  <Text fontSize="2xl">{todo}</Text>
+                </Checkbox>
+              </HStack>
+            </ListItem>
+          ))}
+        </List>
+      </VStack>
+    </Box>
   );
 }
 
